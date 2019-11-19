6670cbf136e00685ad1bd4e34f6fb7dc8f334c5c
Paweł Kaczmarek <pawel.kaczmarek95@gmail.com>
2019-11-19 22:05:11 +0100

    Add slideNav component to form steps

--- src/components/roiCalcForm/steps/step3/step3.component.js
+++ src/components/roiCalcForm/steps/step3/step3.component.js
@@ -1,17 +1,47 @@
 import React from 'react';
+import SlideNav from '../../../slideNav/slideNav.component';
+import { Button } from '../../../button/button.styled';
 
+import {
+  StepWrap,
+  Title,
+  Text,
+} from './step3.styled';
 class Step3 extends React.Component {
 
   render() {
     const {
       currentStep,
+      onNextStep,
     } = this.props;
 
     if (currentStep !== 3)
       return null;
 
     return(
-      <div>Step3</div>
+      <StepWrap>
+        <Title>
+          LIVE PPV EVENT ROI CALCULATOR
+        </Title>
+        <Text>
+          Working with a Live PPV specialist can save you a lot of time and development costs.
+        </Text>
+        <Text>
+          When you are building your monetization platform, you consider the
+          costs of design, streaming, along with landing page and e-commerce 
+          checkout development.
+        </Text>
+        <Text>
+          What level of streaming quality are you aiming for?
+        </Text>
+        <Button
+          onClick={ () => onNextStep() }
+          center
+        >
+          See the results
+        </Button>
+        <SlideNav active={ currentStep } stepsNum="3" />
+      </StepWrap>
     )
   }
 }
